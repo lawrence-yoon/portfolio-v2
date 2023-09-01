29decb719de85a768357172c66da37bd32171462
larr <sadpandaemoji@gmail.com>
2023-09-01 09:05:06 -0400

    apply new solution for underline in nav

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -10,37 +10,37 @@ export default function Navbar() {
       <nav className="order-3 md:order-2">
         <ul className="flex flex-col gap-1 items-end p-4 md:p-6 md:gap-4 md:flex-row md:items-center md:justify-center">
           <li
-            className={`border-transparent border-b-2 ${
+            className={`border-b-2 ${
               pathname === "/about"
                 ? "text-[2rem] leading-5 border-slate-500 border-b-8"
-                : "text-gray-500 md:hover:border-gray-500 md:hover:leading-3"
+                : "border-transparent text-gray-500 md:hover:border-gray-500 md:hover:leading-3"
             }`}
           >
             <Link href="/about">about</Link>
           </li>
           <li
-            className={`border-transparent border-b-2 ${
+            className={`border-b-2 ${
               pathname === "/projects"
                 ? "text-[2rem] leading-5 border-slate-500 border-b-8"
-                : "text-gray-500 md:hover:border-gray-500 md:hover:leading-3"
+                : "border-transparent text-gray-500 md:hover:border-gray-500 md:hover:leading-3"
             }`}
           >
             <Link href="/projects">projects</Link>
           </li>
           <li
-            className={`border-transparent border-b-2 ${
+            className={`border-b-2 ${
               pathname.includes("/blog")
                 ? "text-[2rem] leading-5 border-slate-500 border-b-8"
-                : "text-gray-500 md:hover:border-gray-500 md:hover:leading-3"
+                : "border-transparent text-gray-500 md:hover:border-gray-500 md:hover:leading-3"
             }`}
           >
             <Link href="/blog">blog</Link>
           </li>
           <li
-            className={`border-transparent border-b-2 ${
+            className={`border-b-2 ${
               pathname === "/contact"
                 ? "text-[2rem] leading-5 border-slate-500 border-b-8"
-                : "text-gray-500 md:hover:border-gray-500 md:hover:leading-3"
+                : "border-transparent text-gray-500 md:hover:border-gray-500 md:hover:leading-3"
             }`}
           >
             <Link href="/contact">contact</Link>
